903498cf7007fe69298d47c0511c1fb2d3864c60
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-01-10 16:49:43 +0100

    Format src/rebar3_shine.erl

--- src/rebar3_shine.erl
+++ src/rebar3_shine.erl
@@ -10,30 +10,30 @@
 %% ===================================================================
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
-    Provider = providers:create([
-            {name, ?PROVIDER},            % The 'user friendly' name of the task
-            {module, ?MODULE},            % The module implementation of the task
-            {bare, true},                 % The task can be run by the user, always true
-            {deps, ?DEPS},                % The list of dependencies
-            {example, "rebar3 shine"}, % How to use the plugin
-            {opts, []},                   % list of options understood by the plugin
-            {short_desc, "A rebar plugin"},
-            {desc, "A rebar plugin"}
-    ]),
+    Provider =
+        providers:create([{name, ?PROVIDER},            % The 'user friendly' name of the task
+                          {module, ?MODULE},            % The module implementation of the task
+                          {bare,
+                           true},                 % The task can be run by the user, always true
+                          {deps, ?DEPS},                % The list of dependencies
+                          {example, "rebar3 shine"}, % How to use the plugin
+                          {opts, []},                   % list of options understood by the plugin
+                          {short_desc, "A rebar plugin"},
+                          {desc, "A rebar plugin"}]),
     {ok, rebar_state:add_provider(State, Provider)}.
-
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    rebar_gleam:provider_do(State, fun (State1) ->
-      compile:file("gen/test/shine_test"),
-      Pass = fun shine_test:passing/0,
-      Fail = fun shine_test:failing/0,
+    rebar_gleam:provider_do(State,
+                            fun(State1) ->
+                               compile:file("gen/test/shine_test"),
+                               Pass = fun shine_test:passing/0,
+                               Fail = fun shine_test:failing/0,
 
-      shine:run_suite([{"shine_test", [Pass, Fail]}]),
-      {ok, State1}
-    end).
+                               shine:run_suite([{"shine_test", [Pass, Fail]}]),
+                               {ok, State1}
+                            end).
 
--spec format_error(any()) ->  iolist().
+-spec format_error(any()) -> iolist().
 format_error(Reason) ->
     io_lib:format("~p", [Reason]).
