607d7924022edab1d3ebea6dfdf9bc9e7b212013
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-01-13 22:59:34 +0100

    Switch to test profile (#18)
    * Remove generator comments from rebar3_shine
    
    * Use test profile in rebar3_shine

--- src/rebar3_shine.erl
+++ src/rebar3_shine.erl
@@ -11,15 +11,15 @@
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
     Provider =
-        providers:create([{name, ?PROVIDER},            % The 'user friendly' name of the task
-                          {module, ?MODULE},            % The module implementation of the task
-                          {bare,
-                           true},                       % The task can be run by the user, always true
-                          {deps, ?DEPS},                % The list of dependencies
-                          {example, "rebar3 shine"},    % How to use the plugin
-                          {opts, []},                   % list of options understood by the plugin
+        providers:create([{name, ?PROVIDER},
+                          {module, ?MODULE},
+                          {bare, true},
+                          {deps, ?DEPS},
+                          {example, "rebar3 shine"},
+                          {opts, []},
                           {short_desc, "A rebar plugin"},
-                          {desc, "A rebar plugin"}]),
+                          {desc, "A rebar plugin"},
+                          {profiles, [test]}]),
     {ok, rebar_state:add_provider(State, Provider)}.
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
